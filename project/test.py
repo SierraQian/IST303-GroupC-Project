0fcef7fe8243d3e9bacb35308c80bb928aac5634
jy01294776 <chenxu0356@gmail.com>
2023-07-16 13:35:00 -0700

    test file

---
+++ project/test.py
@@ -0,0 +1,52 @@
+import unittest
+from flask import Flask
+from flask_login import current_user
+from project import create_app, db
+from project.models import User
+
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        # Create a test Flask app and configure it for testing
+        self.app = create_app()
+        self.app.config['TESTING'] = True
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+
+        # Create the test database and tables
+        db.create_all()
+
+    def tearDown(self):
+        # Remove the test database and tables
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_index_route(self):
+        # Test the index route
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Welcome to the Home Page', response.data)
+
+    def test_login(self):
+        # Create a test user
+        user = User(email='test@example.com', name='Test User', password='testpassword')
+        db.session.add(user)
+        db.session.commit()
+
+        # Test the login route with correct credentials
+        response = self.client.post('/login', data={'email': 'test@example.com', 'password': 'testpassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(current_user.name, 'Test User')
+
+        # Test the login route with incorrect credentials
+        response = self.client.post('/login', data={'email': 'test@example.com', 'password': 'wrongpassword'})
+        self.assertEqual(response.status_code, 302)
+        self.assertNotEqual(current_user.name, 'Test User1')
+
+    # Add more tests for other routes and functionalities
+
+if __name__ == '__main__':
+    unittest.main()
